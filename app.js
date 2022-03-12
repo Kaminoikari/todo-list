d0e8a84e9367e4fd9c526a62b490e6c030940cff
Charles <charles0527@hotmail.com>
2022-03-12 19:59:27 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const bodyParser = require('body-parser') // 載入 body-parser
 const Todo = require ('./models/todo') // 載入 Todo model
-const express = require('express');
-const mongoose = require('mongoose');
-const exphbs = require ('express-handlebars');
+const express = require('express')
+const exphbs = require ('express-handlebars')
+
 const methodOverride = require ('method-override')
 
 // 引用路由器
 const routes = require('./routes')
+require('./config/mongoose') // Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
+
 const app = express();
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine ('hbs', exphbs ({ defaultLayout: 'main', extname: '.hbs'}))
 app.set ('view engine', 'hbs')
