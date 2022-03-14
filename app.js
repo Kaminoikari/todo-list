97547ca592ecec73319d95faf38fbf8c33970138
Charles <charles0527@hotmail.com>
2022-03-14 18:23:37 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const routes = require('./routes')
 require('./config/mongoose') 
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.engine ('hbs', exphbs ({ defaultLayout: 'main', extname: '.hbs'}))
 app.set ('view engine', 'hbs')
