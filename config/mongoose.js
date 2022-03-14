aff169a6e2839e49f5f1d810ef1a86c19f3d5235
Charles <charles0527@hotmail.com>
2022-03-14 19:16:14 +0800

    fix: mongoose uri setting

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 // 如果在 Heroku 環境則使用 process.env.MONGODB_URI
 // 否則為本地環境，使用 mongodb://localhost/todo-list
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
